4d28ae21bd88854c6d9f17fefd39b83d58855b6f
e0marina <emilymarinaro@gmail.com>
2021-02-27 15:34:34 -0800

    added openweather

--- map/src/StateContainer/stateContainer.js
+++ map/src/StateContainer/stateContainer.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import Map from '../Map/map.js';
 
 
-
+//to do: on load, have weather api called with the lat and long
 class StateContainer extends Component {
     state = {
-        lat: 51.505,
-        lng: -0.09,
-        zoom: 13,
+        lat: 42.2808,
+        lng: -83.7430,
+        zoom: 14,
 
     }
+    componentDidMount() {
+        fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + this.state.lat + "&lon=" + this.state.lng + "&appid=c3b781a7de141c8e06094287b67539ce&units=imperial")
+            .then(function (res) {
+                // console.log(res.json())
+                return res.json();
+            }).then(function (data) {
+                console.log(data)
+            })
+    }
+
+
     render() {
         const position = [this.state.lat, this.state.lng]
 
