3fe913e1581f0cc837875c2a8987cc27c6c8efd2
e0marina <emilymarinaro@gmail.com>
2021-03-01 11:28:41 -0800

    lat and long updating when city entered, now need to get the map to change places

--- map/src/StateContainer/stateContainer.js
+++ map/src/StateContainer/stateContainer.js
@@ -1,43 +1,43 @@
 import React, { Component } from "react";
 import Map from '../Map/map.js';
 
-
-//to do: have form where user can add their location
 class StateContainer extends Component {
     state = {
         lat: 42.2808,
         lng: -83.7430,
         zoom: 14,
         value: '',
-
     }
 
     componentDidMount() {
+        //call the api on mount to display default map
         fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + this.state.lat + "&lon=" + this.state.lng + "&appid=c3b781a7de141c8e06094287b67539ce&units=imperial")
             .then(function (res) {
                 // console.log(res.json())
                 return res.json();
-            }).then(function (data) {
-                console.log(data);
-
             })
     }
 
     handleChange = (event) => {
         this.setState({ value: event.target.value });
-        //console.log(this.state.value);
     }
 
     handleSubmit = (event) => {
         console.log('A city was submitted: ' + this.state.value);
         event.preventDefault();
+        //call the weather api and get the data by city that was entered.
+        //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=" + this.state.value + "&appid=c3b781a7de141c8e06094287b67539ce&units=imperial")
+            .then((response) => response.json())
+            .then(data => {
+                this.setState({ lat: data.coord.lat, lng: data.coord.lon });
+            });
     }
+
 
 
     render() {
         const position = [this.state.lat, this.state.lng];
-        // console.log(`Weather Data: ${this.state.weatherData}`);
-
         return (
             <div>
                 <Map position={position} zoom={this.state.zoom} />
@@ -49,7 +49,6 @@ class StateContainer extends Component {
                     <input type="submit" value="Submit" />
                 </form>
             </div>
-
         );
     }
 }
